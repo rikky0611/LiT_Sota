f52e7dbd8f6bcd43061ad7f1591b7b0f872c69e4
田中章生 <TanakaAkio@tanakaakio-no-macbook-air.local>
2016-01-20 19:16:48 +0900

    Initial Commit

---
+++ sciorViwe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sciorViwe
+//
+//  Created by 田中章生 on 2016/01/20.
+//  Copyright © 2016年 t_souta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
