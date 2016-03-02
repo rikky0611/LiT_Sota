37d0191e689aac5d4df6197d807bf4d46144aba5
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-03-02 21:27:04 +0900

    initial commit

---
+++ sciorViwe/scrollView.swift
@@ -0,0 +1,21 @@
+//
+//  scrollView.swift
+//  sciorViwe
+//
+//  Created by 田中章生 on 2016/01/27.
+//  Copyright © 2016年 t_souta. All rights reserved.
+//
+
+import UIKit
+
+class scrollView: UIScrollView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
